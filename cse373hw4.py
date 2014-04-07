4a3a290eae8883d912db8c6e845a66eda6423210
kevin qi <kevinkeyjkw@gmail.com>
2014-04-07 17:24:54 -0400

    Slow code but finished

--- cse373hw4.py
+++ cse373hw4.py
@@ -1,9 +1,9 @@
 import sys
-
+import pdb
 
 def isSolution(a):
 	guessArray = [subsets[i] for i in range(len(a)) if a[i]]
-	print(guessArray)
+	#print(guessArray)
 	isSol = False
 	for e in numsToCover:
 		isSol = False
@@ -14,13 +14,68 @@ def isSolution(a):
 		if(not isSol):
 			return False
 	return True
-def processSolution(guessArray):
+
+def printSolution(a):
+	guessArray = [subsets[i] for i in range(len(a)) if a[i]]
+	print(guessArray)
+# 
+# We put a=[T,T,T...,T] -> a=[F,F,F,...] means no more moves left
+# def backtrack(a,k,n):
+# 	c = [0,0]
+# 	ncandidates=2
+# 	if(is_a_solution(a,k,n)):
+# 		process_solution(a,k)
+# 	else:
+# 		k += 1
+# 		if k < n:
+# 			construct_candidates(a,c)
+# 			for i in range(2):
+# 				a[k] = c[i]
+# 				backtrack(a,k,n)
+
+
+def combo(a,k):
+	if k == len(a):
+		if isSolution(a):
+			processSolution(a)
+	else:
+		a[k] = 0
+		combo(a,k+1)
+		a[k] = 1
+		combo(a,k+1)
+		
+	
+def processSolution(a):
+	global minSolution
+	guessArray = [subsets[i] for i in range(len(a)) if a[i]]
 	if len(guessArray) < len(minSolution):
 		minSolution = guessArray
-#print(len(guessArray))
-#def candidates():	
-def minCover(a,k,data):
-	candidates
+
+
+			
+# 	c = [True,False]
+# 	if(k > len(subsets)):
+# 		return
+# 	else:
+# 		if(isSolution(a)):
+# 			processSolution(a)
+# 		k = k + 1
+# 		if k > len(a):
+# 			return
+# 		a[k] = c[0]
+# 		minCover(a,k)
+# 		a[k] = c[1]
+# 		minCover(a,k)
+# 		return
+				
+# 			
+# def test(a):
+# 	c = [0,0]
+# 	test2(c)
+# 	print(c)
+# def test2(b):
+# 	b[0] = True
+# 	b[1] = False
 	
 	
 
@@ -38,13 +93,18 @@ for x in range(1,int(content[0])+1):
 	numsToCover.append(x)
 content = content[2:]
 for element in content:
-	temp = [int(x) for x in element.split(' ')]
+	if element[len(element)-1] == ' ':
+		element = element[:len(element)-1]
+	if(' ' in element):
+		temp = [int(x) for x in element.split(' ')]
+	else:
+		temp = [int(element)]
 	subsets.append(temp)
 minSolution = subsets
-print(isSolution([1,0,0,0,1,0]))
-#print(subsets)
-#if(isSolution([1,2,3,4,5,6],[[1,2,3],[4,5,6]])):
-#	processSolution([[1,2,3],[4,5,6]])
+a = [0 for i in range(len(subsets))]
+
+combo(a,0)
+print(minSolution)
 #Example of variables/
 #subsets = [[1,2,3],[2,3],[3,4,5]]
 #numOfSubsets = 12
