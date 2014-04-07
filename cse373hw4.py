0335a63072afc595400d768dd76e4c50e17c9762
kevin qi <kevinkeyjkw@gmail.com>
2014-04-06 23:16:07 -0400

    2nd

--- cse373hw4.py
+++ cse373hw4.py
@@ -1,8 +1,11 @@
 import sys
 
-def isSolution(numsToCoverArray,guessArray):
+
+def isSolution(a):
+	guessArray = [subsets[i] for i in range(len(a)) if a[i]]
+	print(guessArray)
 	isSol = False
-	for e in numsToCoverArray:
+	for e in numsToCover:
 		isSol = False
 		for element in guessArray:
 			if e in element:
@@ -11,7 +14,17 @@ def isSolution(numsToCoverArray,guessArray):
 		if(not isSol):
 			return False
 	return True
-			
+def processSolution(guessArray):
+	if len(guessArray) < len(minSolution):
+		minSolution = guessArray
+#print(len(guessArray))
+#def candidates():	
+def minCover(a,k,data):
+	candidates
+	
+	
+
+minSolution = []		
 f = open(sys.argv[1],'r')
 content = []
 numsToCover = []
@@ -27,17 +40,18 @@ content = content[2:]
 for element in content:
 	temp = [int(x) for x in element.split(' ')]
 	subsets.append(temp)
+minSolution = subsets
+print(isSolution([1,0,0,0,1,0]))
 #print(subsets)
-print(isSolution([1,2,3,4,5,6],[[1,2],[4,5,6]]))
+#if(isSolution([1,2,3,4,5,6],[[1,2,3],[4,5,6]])):
+#	processSolution([[1,2,3],[4,5,6]])
 #Example of variables/
 #subsets = [[1,2,3],[2,3],[3,4,5]]
 #numOfSubsets = 12
 #numsToCover = [1,2,3,4,5]
 #finished = true
 
-#def minCover(set):
-	
-#def candidates():
+
 
 
 		
